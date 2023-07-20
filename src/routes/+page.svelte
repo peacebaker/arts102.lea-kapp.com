9091e232f1018a00808c942a84616d6d4c1ba593
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-07-20 18:22:53 -0400

    2nd day commit forgot first day

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,168 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
+</svelte:head>
+
+<div class="container">
+  <nav>
+
+    <a href="#first-day">
+      First Day
+    </a>
+    
+
+  </nav>
+
+  <main>
+    <h1 id="first-day">First Day</h1>
+    <p>When I signed up for ARTS 102 last semester, I figured "this is an intro class – it can't be too hard," and signed up for the condensed course over the summer semester;  however, on my first day, I started to panic.  I had heard all of these words before, but in this context, they didn't make sense.  How could art "move" if it's a still image?  Aren't form, space, and shape basically the same thing?</p>
+    <p>Worst of all, our instructor said that we needed a sketchbook, and I could not draw – or at least I thought I couldn't draw.  I had tried drawing on several occasions, and every time, the drawings turned out like garbage.  I can't turn in garbage for school!</p>
+    <div class="iframe-wrapper">
+      <iframe src="https://giphy.com/embed/BbBYfhduY1u3m" title="Stressed Spongebob" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </div>
+    <a href="https://giphy.com/gifs/spongebob-squarepants-stressed-BbBYfhduY1u3m">via GIPHY</a>
+    <p>But the professor said I could use digital tools, which I felt more comfortable with, and my friend had been talking about Krita for the past several months.  I'm already a Linux junkie, and the fact that one of our tools is leaking into the mainstream made me very excited to try it.  In fact, I already needed a new laptop, so I bought one with a touchscreen specifically for this class.</p>
+    <p>Unfortunately, my first attempts were less than stellar.  This is the first drawing I tried to copy from the textbook:</p>
+    <img src="should_be_easy.webp" alt="an attempt to recreate Picasso's War and Peace">
+    [Lea Kapp, 2023]
+
+    <p>I thought it looked easy, but it wasn't.</p>
+    <p>I didn't like the results, and I immediately deflated.  Not only was I going to fail this class, but I had also spent an extra I-don't-want-to-think-about-it dollars on this laptop for the touchscreen, and now I'm never going to use it.  Even with practice, I could never get "good" because I didn't know what to practice.  Why did I even try?  It was a bad night.</p>
+    <p>I guess there is some truth to at least that final statement.  If you don't know what to practice, it's incredibly hard to get better.  Fortunately, millions of artists have come before us, leaving methodology and advice that you can follow to grow as an artist.  In no time, if you follow the Elements of Design and Principles of Design, you can make anything look nice.</p>
+    <p>Author's Note: A keen eye would spot that I purchased a touchscreen laptop to draw digitally, yet all of my drawings are done by hand.  Why is this?  Because when I draw a diagonal, it looks like this:</p>
+
+    <img src="squiggly_line.webp" alt="a squiggly line with two arbitrary break points where the pen cut out">
+    [Lea Kapp, 2023]
+
+    <p>
+      Oh, squiggly line,<br>
+      Yes, you, squiggly line:<br>
+      Line that doth wiggle<br>
+      And jiggle across the page,<br>
+      Why doth thou taunt me?
+    </p>
+
+    <p>
+      For my hand is quite steady.<br>
+      And my ruler quite straight.<br>
+      Yet you, squiggly line,<br>
+      Are not.
+    </p>
+
+    <h1>Elements of Design</h1>
+    <p>The elements of design are the tools we can use as artists/designers in order to achieve the effects we want.  Many of them are based on English words, but their definitions shift in the context of design.  By utilizing these elements in certain ways, we can create effects that will be universally interpretable, or at least pretty close.  The elements of design are:</p>
+    <ul>
+      <li>Line</li>
+      <li>Texture</li>
+      <li>Shape</li>
+      <li>Form</li>
+      <li>Space</li>
+      <li>Value</li>
+      <li>Color</li>
+    </ul>
+
+    <h2>Line</h2>
+    <p>My favorite definition for a line that I came across was, "a moving point made by a pointed tool, such as a brush, pen, or stick."  I think the definition is a bit limiting, but it certainly paints a clear picture.  I originally learned about lines in math class, and they had some pretty strict rules about what makes a line a line:</p>
+    <ul>
+      <li>Must be straight.</li>
+      <li>Goes on forever in both directions.</li>
+      <li>Probably has a slope or something.</li>
+    </ul>
+    <p>But those rules don't exist in art class.  In art class, a line is simply a boundary between two things, usually shapes.  Lines can be actual, like the outline of a cartoon character, or implied, like when you shift between colors with no gradient.</p>
+    <img src="powerpuff_girls.webp" alt="the powerpuff girls logo">
+    [Cartoon Network, 1998]
+
+    <h2>Texture</h2>
+    <p>Texture describes how a surface feels, or how it would feel if you were allowed to touch it without getting kicked out of the art gallery.  Do not touch the art without explicit permission.  They do not like that.  Like lines, textures don't need to be real – they can also be implied.  When texture describes something intangible, it describes how we imagine it would feel if it were to become a tangible object.</p>
+
+    [Transition between carpet and hardwood in the kitchen.]
+
+    <h2>Shape</h2>
+    <p>A shape is a 2D area that is bound by line, value or color.  It can also be thought of as any flat, enclosed area.  Because the area is 2-Dimensional, all shapes will have a length and a width.  Artists and designers use a combination of geometric and organic shapes to create designs.</p>
+    <img src="2d_shapes.webp" alt="a cyan circle, a spring green triangle, and a violet rectangle, arranged vaguely in the shape of a face">
+    [Lea Kapp, 2023]
+
+    <h2>Form</h2>
+    <p>Form is a 3D area that's bound by line, value, or color.  It's almost the same thing as shape, but 3D instead of 2D.  Since forms are 3D, they have length, they have length, width, and depth.</p>
+    <div class="iframe-wrapper">
+      <iframe src="https://www.youtube-nocookie.com/embed/WhWc3b3KhnY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    
+    [Blender Studio, 2019]
+
+    <h2>Space</h2>
+    <p>Space is the distance between objects.  It can be used to create the illusion of depth within a piece of art, or sometimes, the space might be part of the art.  Space can be 2D, 3D, negative, or positive.  Negative space is often viewed as the "background" whereas positive space is usually perceived as being in the "foreground."</p>
+    
+    [probably a perspective drawing?]
+
+    <h2>Value</h2>
+    <p>Value is an aspect of color that describes lightness or darkness, and the difference between values is called value contrast.  By adding white to a color, we lighten it, creating a tint; and by adding black, we darken it, creating a shade.  By adding gray, we can create what's called a tone, and that's usually a good idea since pure colors are very bright.</p>
+
+    [sliding scale of value]
+
+    <h2>Color</h2>
+    <p>According to physics, what we see as color is the byproduct of neurons firing into the biochemical-electrical soup that is the human brain in response to your retinas coming in contact with electromagnetic waves of varying wavelengths within the spectrum of 430 to 790 terahertz.  In order from lower frequency to higher frequency:</p>
+    <img src="linear_visible_spectrum.svg" alt="depiction of the visible light spectrum">
+
+    <p>To mix colors, we specify how much red, green, and blue we want on a scale of 0-255.  Why 0-255?  Remember when I said each color has a channel of 8 bits?  8^2 = 256, so that's how much red, green, and blue we have to work with.  In the old days, we had to use hexadecimal notation, and you'll still see that everywhere, but modern day CSS3 allows for the rgb() syntax, so there's no reason for that nonsense except showing off.</p>
+    
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>RGB</th>
+        <th>Hex</th>
+        <th>HSL</th>
+        <th>Color</th>
+      </tr>
+      
+      <tr>
+        <td></td>
+      </tr>
+    </table>
+    [Couple examples of rgb colors.  Probably in a table, showing hex codes as well?]
+
+    <p>However, these colors aren't so good to work with unless you plan on blinding someone, so you'll almost always want to desaturate them.  Since this requires some fancy-pants math, and no one likes math, I usually recommend using HSL notation.  HSL stands for Hue, Saturation, and Lightness (value), and you can set each of these values individually.</p>
+    <p>There are 360 hues, which represent values around the RGB color wheel using angular degrees.  0/360 is red, 120 is green, and 240 is blue.  Both saturation and lightness can be set to values between 0 and 100%, and the browser does all the math for you.  Since 0% saturation is gray, 100% lightness is white, and 0% lightness is black, the color spectrum is slightly more limited, there are only 3,564,000 colors to choose from.</p>
+
+  </main>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0px;
+    font-family: "Fira Sans";
+    --white: hsl(0, 0%, 95%);
+    --black: hsl(0, 0%, 5%);
+    /* background-color: var(--black);
+    color: var(--white); */
+    /* font-size: 1.1em; */
+  }
+  img {
+    width: 100vw;
+  }
+
+  .iframe-wrapper {
+    position: relative;
+    box-sizing: content-box;
+    padding-bottom: 56.25%; /* 16:9 aspect ratio */
+  }
+
+  .iframe-wrapper iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .container {
+    display: grid;
+    grid-template-areas:
+      "nav"
+      "main"
+  }
+
+  nav {
+
+  }
+
+</style>
